7fdaddb1ac451247bab9c2053a40a68b27e2f8d4
Gergely Imreh <imrehg@gmail.com>
2011-02-10 23:39:44 +0800

    php: install script

---
+++ php/php.sh
@@ -0,0 +1,46 @@
+PVER=5.3.5
+wget -c http://nl.php.net/get/php-$PVER.tar.bz2/from/nl.php.net/mirror
+tar xjf php-$PVER.tar.bz2
+cd php-$PVER
+
+./configure \
+	--prefix=/opt/php5 \
+	--with-config-file-path=/opt/php5/etc \
+	--with-curl \
+	--with-pear \
+	--with-gd \
+	--with-jpeg-dir \
+	--with-png-dir \
+	--with-zlib \
+	--with-xpm-dir \
+	--with-freetype-dir \
+	--with-mcrypt \
+	--with-mhash \
+	--with-mysql \
+	--with-mysqli \
+	--with-pdo-mysql \
+	--with-openssl \
+	--with-xmlrpc \
+	--with-xsl \
+	--with-bz2 \
+	--with-gettext \
+	--with-fpm-user=web \
+	--with-fpm-group=www-data \
+	--enable-fpm \
+	--enable-exif \
+	--enable-wddx \
+	--enable-zip \
+	--enable-bcmath \
+	--enable-calendar \
+	--enable-ftp \
+	--enable-mbstring \
+	--enable-soap \
+	--enable-sockets \
+	--enable-sqlite-utf8 \
+	--enable-shmop \
+	--enable-dba \
+	--enable-sysvmsg \
+	--enable-sysvsem \
+	--enable-sysvshm \
+
+make
