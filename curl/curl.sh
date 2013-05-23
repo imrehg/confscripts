2d736ece84d89b5905626bcc002c3f09dc47d47f
Gergely Imreh <imrehg@gmail.com>
2013-05-23 11:36:58 +0200

    add curl

---
+++ curl/curl.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+VERSION=7.30.0
+FILENAME=curl-${VERSION}.tar.bz2
+URL=http://curl.haxx.se/download/${FILENAME}
+wget -c ${URL}
+tar xjvf ${FILENAME}
+cd curl-${VERSION}
+
+./configure \
+	--with-ssl \
+
+make
