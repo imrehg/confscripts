5a8ac4728351907ae957809ad600818eeb2105dd
Gergely Imreh <imrehg@gmail.com>
2014-05-19 07:08:09 +0200

    ngx_pagespeed 1.8.31.2-beta

---
+++ ngx_pagespeed/pagespeed.sh
@@ -0,0 +1,9 @@
+VER=1.8.31.2
+GHVER=${VER}-beta
+
+wget -c https://github.com/pagespeed/ngx_pagespeed/archive/v${GHVER}.zip
+unzip v${GHVER}.zip
+
+cd ngx_pagespeed-${GHVER}/
+wget https://dl.google.com/dl/page-speed/psol/${VER}.tar.gz
+tar -xzvf ${VER}.tar.gz
